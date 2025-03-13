2d2c1291cb8e1fb0c4025e56976ec685a85a9b7d
Cameron Clough <cameronjclough@gmail.com>
2025-03-13 23:39:45 +0000

    revert to hex

--- src/gpt.js
+++ src/gpt.js
@@ -203,9 +203,9 @@ function checkHeaderCrc(gptData, guidGpt) {
   const testView = new DataView(testHeader.buffer);
 
   const headerCrc = guidGpt.header.crc32;
-  const testHeaderCrc = testView.getUint32(16, true);
+  const testHeaderCrc = testView.getUint32(0x10, true);
   const partTableCrc = guidGpt.header.crc32PartEntries;
-  const testPartTableCrc = testView.getUint32(88, true);
+  const testPartTableCrc = testView.getUint32(0x58, true);
 
   return [(headerCrc !== testHeaderCrc) || (partTableCrc !== testPartTableCrc), partTableCrc];
 }
